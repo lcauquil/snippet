6728d4979ebb0c1268a1b880f2062aa755d3df67
Laurent Cauquil <laurent.cauquil@inra.fr>
2020-11-20 09:13:19 +0100

    change file name

---
+++ save-3D-rgl-plot/View_spec.R
@@ -0,0 +1,11 @@
+list(FOV = 30, ignoreExtent = FALSE, listeners = 6L, mouseMode = c(left = "trackball", 
+right = "zoom", middle = "fov", wheel = "pull"), skipRedraw = FALSE, 
+    userMatrix = structure(c(0.593086540699005, 0.168518453836441, 
+    0.787305533885956, 0, 0.788759887218475, 0.0746241509914398, 
+    -0.610155045986176, 0, -0.161574363708496, 0.982869744300842, 
+    -0.0886620432138443, 0, 0, 0, 0, 1), .Dim = c(4L, 4L)), userProjection = structure(c(1, 
+    0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1), .Dim = c(4L, 
+    4L)), scale = c(1.17337143421173, 1.76005697250366, 0.715955436229706
+    ), viewport = c(x = 0L, y = 0L, width = 946L, height = 649L
+    ), zoom = 1, windowRect = c(190L, 213L, 1136L, 862L), family = "sans", 
+    font = 1L, cex = 1, useFreeType = FALSE)
